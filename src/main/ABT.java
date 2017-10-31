baac6ad2e58bb90f3b8ef8bea7f625cd0874d116
flouMicaza <flo.mirandai@gmail.com>
2017-10-30 23:09:08 -0300

    Constructores del nodo creados

--- src/main/ABT.java
+++ src/main/ABT.java
@@ -1,10 +1,6 @@
 package main;
 
-
-
-
-
 //Arbol de busqueda ternario
 public class ABT {
-
+//TODO hacer todo
 }
--- src/main/ABTNode.java
+++ src/main/ABTNode.java
@@ -10,20 +10,41 @@ import java.util.ArrayList;
  */
 
 public class ABTNode {
+  String etiqueta;
   String key; //llave del diccionario
   ArrayList<Integer> values; //valores asociados a la llave
   ABTNode izq;
   ABTNode der;
-  ABTNode centro;
-  ABTNode padre; //porsiacaso se necesita para algun arbol
+  ABTNode center;
+  ABTNode father; //porsiacaso se necesita para algun arbol
+  int isLeaf;
   
-  
-  /**Contructor de un nodo. 
+  /**Contructor de un nodo.
+   * Solo cuando no es una hoja
    * 
    */
-  public void Nodo(){
-    //TODO constructor nodo
+  public void Nodo(ABTNode father,String etiqueta){
+    this.isLeaf=0; //este es el constructor para no hojas
+    this.izq=null;
+    this.der=null;
+    this.center=null;
+    this.father=father;
+    this.etiqueta=etiqueta;
+    
   }
 
-
+  /**Contructor de un nodo.
+   * Solo cuando es una hoja
+   * 
+   */public void Nodo(ABTNode father,String etiqueta,String key,int value){
+    this.isLeaf=1; //este es el constructor para hojas
+    this.izq=null;
+    this.der=null;
+    this.center=null;
+    this.father=father;
+    this.etiqueta=etiqueta;
+    this.key=key;
+    this.values.add(value);
+     
+  }
 }
