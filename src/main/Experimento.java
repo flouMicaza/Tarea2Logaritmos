cc920d04c2386b51a23da3359c7a8718892c6fd3
kreeshx <crisep95@gmail.com>
2017-11-08 01:29:41 -0300

    Se terminan las busquedas e insercion en los experimentos

--- src/main/Experimento.java
+++ src/main/Experimento.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
 
 public class Experimento {
 
-	  public static void generarABTs(int n, int i, String[] palabras, ABT arbol, PrintWriter printerABT, PrintWriter printerABTTotal){
+	  public static void generarABTs(int n, int i, String[] palabras, Arbol arbol, PrintWriter printerABT, PrintWriter printerABTTotal){
 	    float timeABT = 0;
 	    while (i < n) {
 	      long timeIni = System.nanoTime();
@@ -27,7 +27,7 @@ public class Experimento {
 		printerABTTotal.write(System.lineSeparator());
 	  }
 	
-	  private static void buscarABTs(int n, int i, String[] palabras, ABT arbol, PrintWriter printerABT, PrintWriter printerABTTotal) {
+	  private static void buscarABTs(int n, int i, String[] palabras, Arbol arbol, PrintWriter printerABT, PrintWriter printerABTTotal) {
 		  float timeABT = 0;
 		  while (i < n) {
 			  Random rand = new Random();
@@ -71,17 +71,32 @@ public class Experimento {
 	    System.out.println(palabras.length);
 	    int i = 0;
 	    ABT arbolABT = new ABT(palabras[0],i);
+	    HashTable arbolHT = new HashTable();
+	    arbolHT.insertar(palabras[0], i);
+	    Patricia arbolPT = new Patricia(palabras[0],i);
 	    i++;
 		printerABTTotal1 = new PrintWriter("/Users/user/Desktop/ABTInsertar.txt", "UTF-8");
 		printerABTTotal2 = new PrintWriter("/Users/user/Desktop/ABTBuscar.txt", "UTF-8");
+		printerPTTotal1 = new PrintWriter("/Users/user/Desktop/PTInsertar.txt", "UTF-8");
+		printerPTTotal2 = new PrintWriter("/Users/user/Desktop/PTBuscar.txt", "UTF-8");
+		printerHTTotal1 = new PrintWriter("/Users/user/Desktop/HTInsertar.txt", "UTF-8");
+		printerHTTotal2 = new PrintWriter("/Users/user/Desktop/HTBuscar.txt", "UTF-8");
 	    for (Integer each: indexes) {
 		    	try {
 		    		printerABT1 = new PrintWriter("/Users/user/Desktop/ABTInsertar" + each + ".txt", "UTF-8");
 		    		printerABT2 = new PrintWriter("/Users/user/Desktop/ABTBuscar" + each + ".txt", "UTF-8");
-		    		System.out.println("Se ha empezado la inserción en el arbol con " + each + " palabras");
+		    		printerPT1 = new PrintWriter("/Users/user/Desktop/PTInsertar" + each + ".txt", "UTF-8");
+		    		printerPT2 = new PrintWriter("/Users/user/Desktop/PTBuscar" + each + ".txt", "UTF-8");
+		    		printerHT1 = new PrintWriter("/Users/user/Desktop/HTInsertar" + each + ".txt", "UTF-8");
+		    		printerHT2 = new PrintWriter("/Users/user/Desktop/HTBuscar" + each + ".txt", "UTF-8");
+		    		System.out.println("Se ha empezado la inserción en los arboles con " + each + " palabras");
 		    		generarABTs(each, i, palabras, arbolABT, printerABT1, printerABTTotal1);
-		    		System.out.println("Se ha empezado la búsqueda en el arbol con " + each + " palabras");
+		    		//generarABTs(each, i, palabras, arbolHT, printerHT1, printerHTTotal1);
+		    		//generarABTs(each, i, palabras, arbolPT, printerPT1, printerPTTotal1);
+		    		System.out.println("Se ha empezado la búsqueda en los arboles con " + each + " palabras");
 		    		buscarABTs(each, i, palabras, arbolABT, printerABT2, printerABTTotal2);
+		    		//buscarABTs(each, i, palabras, arbolHT, printerHT2, printerHTTotal2);
+		    		//buscarABTs(each, i, palabras, arbolPT, printerPT2, printerPTTotal2);
 		    		printerABT1.close();
 		    		printerABT2.close();
 		    	}
